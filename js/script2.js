b6ff18a65d8c7df73f9cc747762427e04780f034
AjiBanyu <ajibanyu15@gmail.com>
2021-07-02 15:14:35 +0700

    merubah style css, merubah index.html dan menambah img

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,7 @@
-const menuToggle = document.querySelector('.nav__input');
-const nav = document.querySelector('.nav__list');
+// const menuToggle = document.getElementById('nav-input');
+// const nav = document.getElementById('nav-list');
 
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav__slide')
-    // console.log('slide berhasil');
-})
+// menuToggle.addEventListener('click', () => {
+//     nav.classList.toggle('show__menu')
+//     // console.log('slide berhasil');
+// })
